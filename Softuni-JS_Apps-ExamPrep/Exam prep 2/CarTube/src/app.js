5c875452fc810f4bb932c8dc0806351c68f4aac0
Ivan Yankov <avatar8221@gmail.com>
2021-03-31 16:13:51 +0300

    Exam prepping 31-03 at 4.13 PM ... from Laptop

--- Softuni-JS_Apps-ExamPrep/Exam prep 2/CarTube/src/app.js
+++ Softuni-JS_Apps-ExamPrep/Exam prep 2/CarTube/src/app.js
@@ -1,11 +1,10 @@
 import { render } from '../node_modules/lit-html/lit-html.js';
 import page from '../node_modules/page/page.mjs';
 
-import * as api from './api/data.js'
+import { logout as apiLogout } from './api/data.js'
 import { getUserData } from './utility.js';
 import { homePage } from './views/home.js';
 
-// import { logout as apiLogout } from './api/data.js'
 // import { catalogPage } from "./views/catalog.js";
 // import { createPage } from "./views/create.js";
 // import { detailsPage } from "./views/details.js";
@@ -16,10 +15,9 @@ import { homePage } from './views/home.js';
 // import { profilePage } from "./views/profile.js";
 // import { notify } from "./notification.js";
 
-window.api = api;
 
 const main = document.getElementById('site-content');
-// document.getElementById('logoutBtn').addEventListener('click', logout);
+document.getElementById('logoutBtn').addEventListener('click', logout);
 setUserNav();
 
 // 
@@ -47,24 +45,26 @@ page.start();
 function decorateContext(ctx, next) {
 	ctx.render = (content) => render(content, main);
 	ctx.setUserNav = setUserNav;
-    ctx.user = getUserData();
+	ctx.user = getUserData();
 	next();
 }
 
 function setUserNav() {
 	const user = getUserData()
-	if (user != null) {
-        document.getElementById('profile').style.display = '';
+	if (user) {
+		document.getElementById('profile').style.display = 'block';
 		document.getElementById('guest').style.display = 'none';
-		document.getElementById('user-greeting').textContent = `Welcome, ${user.username}`
+		document.getElementById('user-greeting').textContent = `Welcome ${user.username}`
 	} else {
 		document.getElementById('profile').style.display = 'none';
-		document.getElementById('guest').style.display = '';
+		document.getElementById('guest').style.display = 'block';
 	}
 }
 
-// async function logout() {
-// 	await apiLogout();
-// 	setUserNav();
-// 	page.redirect('/');
-// }
+function logout() {
+	apiLogout();
+	setUserNav();
+	page.redirect('/');
+}
+
+
